f858f2fe27744678baee2929bbc21a960968dbad
erickmanaroulas <emanaroulasfelip@apple.com>
2023-01-31 10:42:14 +0100

    add StudentRepository.java

--- src/main/java/br/com/dea/management/student/domain/Student.java
+++ src/main/java/br/com/dea/management/student/domain/Student.java
@@ -11,7 +11,7 @@ public class Student {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column
     private String university;
