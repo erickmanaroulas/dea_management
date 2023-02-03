80589ce901234fb4b22b43e9a6b7123d373365e8
erickmanaroulas <emanaroulasfelip@apple.com>
2023-02-03 11:00:14 +0100

    add logs and update user table name

--- src/main/java/br/com/dea/management/student/controller/StudentController.java
+++ src/main/java/br/com/dea/management/student/controller/StudentController.java
@@ -5,6 +5,7 @@ import br.com.dea.management.student.dto.StudentDto;
 import br.com.dea.management.student.service.StudentService;
 import br.com.dea.management.user.domain.User;
 import br.com.dea.management.user.dto.UserDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/student")
+@Slf4j
 public class StudentController {
 
     @Autowired
@@ -32,15 +34,25 @@ public class StudentController {
     @GetMapping("/all-dto")
     public Page<StudentDto> getStudents(@RequestParam Integer page,
                                         @RequestParam Integer pageSize) {
+        log.info(String.format("Fetching students : page : %s : pageSize", page, pageSize));
 
         Page<Student> studentsPaged = this.studentService.findAllStudentsPaginated(page, pageSize);
         Page<StudentDto> students = studentsPaged.map(student -> StudentDto.fromStudent(student));
+
+        log.info(String.format("Students loaded successfully : Students : %s : pageSize", students.getContent()));
+
         return students;
 
     }
 
     @GetMapping("/{id}")
     public StudentDto getStudent(@PathVariable("id") Integer id) {
-        return StudentDto.fromStudent(this.studentService.findStudentById(id.longValue()));
+        log.info(String.format("Fetching student by id : Id : %s", id));
+
+        StudentDto student = StudentDto.fromStudent(this.studentService.findStudentById(id.longValue()));
+
+        log.info(String.format("Student loaded successfully : Student : %s", student));
+
+        return student;
     }
 }
--- src/main/java/br/com/dea/management/user/domain/User.java
+++ src/main/java/br/com/dea/management/user/domain/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
+@Table(name = "dea_user")
 @NamedQuery(name = "findByName", query = "SELECT u FROM User u where u.name = :name")
 @Getter
 @Setter
