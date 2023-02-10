93c71205ae677a877d3dd7982cba831f490ab4ae
erickmanaroulas <emanaroulasfelip@apple.com>
2023-02-10 11:21:04 +0100

    add swagger documentation to UserController

--- src/main/java/br/com/dea/management/user/controller/UserController.java
+++ src/main/java/br/com/dea/management/user/controller/UserController.java
@@ -5,6 +5,9 @@ import br.com.dea.management.student.dto.StudentDto;
 import br.com.dea.management.user.domain.User;
 import br.com.dea.management.user.dto.UserDto;
 import br.com.dea.management.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +18,13 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @Operation(summary = "Load the list of students paginated.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "400", description = "Page or Page Size params not valid"),
+            @ApiResponse(responseCode = "500", description = "Error fetching student list"),
+    })
+    @GetMapping("/all")
     @RequestMapping(value= "/all", method = RequestMethod.GET)
     public Page<UserDto> getUsers(@RequestParam Integer page,
                                   @RequestParam Integer pageSize) {
@@ -24,6 +34,13 @@ public class UserController {
         return users;
     }
 
+    @Operation(summary = "Load the student by ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "400", description = "Student Id invalid"),
+            @ApiResponse(responseCode = "404", description = "Student Not found"),
+            @ApiResponse(responseCode = "500", description = "Error fetching student list"),
+    })
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public UserDto getUserById(@PathVariable("id") Integer id) {
         return UserDto.fromUser(this.userService.findUserById(id));
