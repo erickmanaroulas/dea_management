cd40dd1b66e638ce55b806fe86c517874a15e8d1
erickmanaroulas <emanaroulasfelip@apple.com>
2023-03-08 09:09:34 -0300

    add update, create and delete routes to studentController

--- src/main/java/br/com/dea/management/student/controller/StudentController.java
+++ src/main/java/br/com/dea/management/student/controller/StudentController.java
@@ -1,13 +1,15 @@
 package br.com.dea.management.student.controller;
 
 import br.com.dea.management.student.domain.Student;
+import br.com.dea.management.student.dto.CreateStudentRequestDto;
 import br.com.dea.management.student.dto.StudentDto;
+import br.com.dea.management.student.dto.UpdateStudentRequestDto;
 import br.com.dea.management.student.service.StudentService;
-import br.com.dea.management.user.domain.User;
-import br.com.dea.management.user.dto.UserDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -18,18 +20,30 @@ import java.util.List;
 @RestController
 @RequestMapping("/student")
 @Slf4j
+@Tag(name = "Student", description = "The Student API")
 public class StudentController {
 
     @Autowired
     StudentService studentService;
 
-    @Operation(summary = "Load all students without pagination.")
+    @Operation(summary = "Load the list of students with all attributes.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successful operation"),
             @ApiResponse(responseCode = "500", description = "Error fetching student list"),
     })
     @Deprecated
-    @RequestMapping(value = "/all-without-pagination", method = RequestMethod.GET)
+    @RequestMapping(value = "/all-raw", method = RequestMethod.GET)
+    public List<Student> getStudentsAllRaw() {
+        return this.studentService.findAllStudents();
+    }
+
+    @Operation(summary = "Load all students.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "500", description = "Error fetching student list"),
+    })
+    @Deprecated
+    @RequestMapping(value = "all-without-pagination", method = RequestMethod.GET)
     public List<StudentDto> getStudentsWithOutPagination() {
         List<Student> students = this.studentService.findAllStudents();
         return StudentDto.fromStudents(students);
@@ -42,8 +56,9 @@ public class StudentController {
             @ApiResponse(responseCode = "500", description = "Error fetching student list"),
     })
     @GetMapping("/all")
-    public Page<StudentDto> getStudents(@RequestParam Integer page,
-                                        @RequestParam Integer pageSize) {
+    public Page<StudentDto> getStudents(@RequestParam(required = true) Integer page,
+                                        @RequestParam(required = true) Integer pageSize) {
+
         log.info(String.format("Fetching students : page : %s : pageSize", page, pageSize));
 
         Page<Student> studentsPaged = this.studentService.findAllStudentsPaginated(page, pageSize);
@@ -63,13 +78,63 @@ public class StudentController {
             @ApiResponse(responseCode = "500", description = "Error fetching student list"),
     })
     @GetMapping("/{id}")
-    public StudentDto getStudent(@PathVariable("id") Integer id) {
+    public StudentDto getStudents(@PathVariable Long id) {
+
         log.info(String.format("Fetching student by id : Id : %s", id));
 
-        StudentDto student = StudentDto.fromStudent(this.studentService.findStudentById(id.longValue()));
+        StudentDto student = StudentDto.fromStudent(this.studentService.findStudentById(id));
 
         log.info(String.format("Student loaded successfully : Student : %s", student));
 
         return student;
+
     }
+
+    @Operation(summary = "Create a new Student")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "400", description = "Payload not valid"),
+            @ApiResponse(responseCode = "500", description = "Error creating student"),
+    })
+    @PostMapping("/")
+    public void createStudent(@Valid @RequestBody CreateStudentRequestDto createStudentRequestDto) {
+        log.info(String.format("Creating Student : Payload : %s", createStudentRequestDto));
+
+        Student student = studentService.createStudent(createStudentRequestDto);
+
+        log.info(String.format("Student created successfully : id : %s", student.getId()));
+    }
+
+    @Operation(summary = "Update a Student")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "400", description = "Payload not valid"),
+            @ApiResponse(responseCode = "404", description = "Student not found"),
+            @ApiResponse(responseCode = "500", description = "Error updating student"),
+    })
+    @PutMapping("/{studentId}")
+    public void updateStudent(@PathVariable Long studentId, @Valid @RequestBody UpdateStudentRequestDto updateStudentRequestDto) {
+        log.info(String.format("Updating Student : Payload : %s", updateStudentRequestDto));
+
+        Student student = studentService.updateStudent(studentId, updateStudentRequestDto);
+
+        log.info(String.format("Student created successfully : id : %s", student.getId()));
+    }
+
+    @Operation(summary = "Delete a Student")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "400", description = "Payload not valid"),
+            @ApiResponse(responseCode = "404", description = "Student not found"),
+            @ApiResponse(responseCode = "500", description = "Error deleting student"),
+    })
+    @DeleteMapping("/{studentId}")
+    public void deleteStudent(@PathVariable Long studentId) {
+        log.info(String.format("Removing Student : Id : %s", studentId));
+
+        studentService.deleteStudent(studentId);
+
+        log.info(String.format("Student removed successfully : id : %s", studentId));
+    }
+
 }
